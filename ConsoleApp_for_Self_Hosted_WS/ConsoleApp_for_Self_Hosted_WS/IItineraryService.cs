47cf3ba87153e9553dc874df3acfeafb6409a7c9
ermedimrc <iaboutenmehdi@correo.ugr.es>
2025-10-15 14:33:31 +0200

    add prohect

---
+++ ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/IItineraryService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using ProxyContracts;
+
+[ServiceContract]
+public interface IItineraryService
+{
+    [OperationContract]
+    [WebGet(UriTemplate = "/Itinerary?origin={origin}&destination={destination}",
+            ResponseFormat = WebMessageFormat.Json)]
+    ItineraryDto GetItinerary(string origin, string destination);
+}
