7b5aea4e4b98f58677d134c0df066584dc4745ae
ermedimrc <iaboutenmehdi@correo.ugr.es>
2025-10-15 14:33:31 +0200

    add prohect

---
+++ ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/ProxyClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using ProxyContracts;
+
+public static class ProxyClient
+{
+    private static readonly BasicHttpBinding Binding = new BasicHttpBinding
+    {
+        MaxReceivedMessageSize = 8 * 1024 * 1024,
+        OpenTimeout = TimeSpan.FromSeconds(5),
+        SendTimeout = TimeSpan.FromSeconds(20),
+        ReceiveTimeout = TimeSpan.FromSeconds(20)
+    };
+
+    // ⚠️ TEMPORAL: cambia esta URL cuando tu compi te diga la suya
+    private static readonly EndpointAddress Address =
+        new EndpointAddress("http://localhost:9000/ProxyService");
+
+    private static readonly ChannelFactory<IProxyService> Factory =
+        new ChannelFactory<IProxyService>(Binding, Address);
+
+    public static IProxyService Create() => Factory.CreateChannel();
+}
---
+++ ConsoleApp_for_Self_Hosted_WS/ConsoleApp_for_Self_Hosted_WS/ProxyContracts.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace ProxyContracts
+{
+    [DataContract]
+    public class ItineraryDto
+    {
+        [DataMember] public string StartStation { get; set; }
+        [DataMember] public string EndStation { get; set; }
+        [DataMember] public int WalkToStartSeconds { get; set; }
+        [DataMember] public int BikeSeconds { get; set; }
+        [DataMember] public int WalkToEndSeconds { get; set; }
+        [DataMember] public double TotalKm { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IProxyService
+    {
+        [OperationContract]
+        ItineraryDto GetItinerary(string origin, string destination);
+    }
+}
